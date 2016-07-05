004f0c1d4418427f4d92cedf56c350c5d45f63b1
Martin Devlin <martin.devlin@pearson.com>
2016-07-05 10:38:32 +0100

    Building

--- src/main/groovy/com/pearson/deployment/helpers/Helper.groovy
+++ src/main/groovy/com/pearson/deployment/helpers/Helper.groovy
@@ -1,8 +1,24 @@
 package com.pearson.deployment.helpers
 
+import org.yaml.snakeyaml.Yaml
+import org.json.JSONException;
+import org.json.JSONObject;
+
 class Helper implements Serializable {
   public static dockerRegistry() {
     Map<String, String> env = System.getenv()
     env.get('DOCKER_REGISTRY') ?: "bitesize-registry.default.svc.cluster.local:5000"
+  }
+
+  def yamlToJson (String y) {
+    Yaml yaml= new Yaml();
+    Object obj = yaml.load(y);
+
+    try {
+      return JSONValue.toJSONString(obj);
+    } catch(all) {
+      LOG.severe StackTraceUtils.deepSanitize(all).toString()
+      return null
+    }
   }
 }
