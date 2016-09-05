7de7a6568bdc2ae477c4b9d97c5821eccd30d6a7
Simas Cepaitis <simas.cepaitis@pearson.com>
2016-09-05 15:02:54 +0300

    New way for managing services

--- src/main/groovy/com/pearson/deployment/kubernetes/AbstractKubeManager.groovy
+++ src/main/groovy/com/pearson/deployment/kubernetes/AbstractKubeManager.groovy
@@ -1,8 +1,52 @@
 package com.pearson.deployment.kubernetes
 
-abstract class AbstractKubeManager {
-    AbstractKubeManager() {        
-    }
+import com.pearson.deployment.config.bitesize.DeploymentMethod
+import com.pearson.deployment.config.bitesize.Service
 
-    abstract boolean manage()
+abstract class AbstractKubeManager {
+  AbstractKubeManager() {        
+  }
+
+  abstract boolean manage()
+
+  static AbstractKubeManager getManagerForDeployment(DeploymentMethod deployment, KubeAPI client, Service svc, OutputStream log) {
+    if (deployment.isBlueGreen() ) {
+      return KubeBlueGreenServiceManager(client, svc, log)
+    } else {
+      return KubeServiceManager(client, svc, log)
+    }
+  }
+
+  static List<KubeResourceHandler> collectResources(DeploymentMethod deployment, KubeAPI client, Service svc, OutputStream log) {
+      List<KubeResourceHandler> retval = []
+
+      // TODO: This will need to be refactored
+      if (deployment.isBlueGreen()) {
+          ["blue", "green"].each { color ->
+            s = svc.clone()
+            s.name = "${s.name}-${color}"
+            if (s.external_url) {
+              s.external_url = Helper.addHostPrefix(s.external_url, "-${color}")
+            }
+
+            if (s.isThirdParty()) {
+              retval.add(new KubeThirdpartyHandler(client, svc, log))
+            } else {
+              retval.add(new KubeDeploymentHandler(client, svc,log))
+              retval.add(new KubeServiceHandler(client, svc, log))
+              retval.add(new KubeIngressHandler(client, svc ,log))
+            }
+          }
+          retval.add(new KubeIngressHandler(client, svc, log))
+      } else {
+        if (s.isThirdParty) {
+          retval.add(new KubeThirdpartyHandler(client, svc, log))
+        } else {
+          retval.add(new KubeDeploymentHandler(client, svc, log))
+          retval.add(new KubeServiceHandler(client, svc, log))
+          retval.add(new KubeIngressHandler(client, svc ,log))
+        }
+      }
+      retval
+  }
 }
