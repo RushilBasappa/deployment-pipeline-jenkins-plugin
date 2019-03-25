185b96307ed8ec6df2dcc438c03ec1c6f5d41dbe
Sithmal Kariyawasam <sithmal@gmail.com>
2019-03-25 15:26:34 +0530

    Adding bitesize loader class.

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ VERSION=${1:-0.0.1}
 gradle test
 gradle clean jar jpi
 cp build/libs/deployment-pipeline-plugin.hpi ../jenkins-bootstrap/docker_build/plugins/
-cp build/libs/deployment-pipeline-plugin-${VERSION}-SNAPSHOT.jar ../jenkins-bootstrap/docker_build/jarfiles
+cp build/libs/deployment-pipeline-jenkins-plugin-${VERSION}-SNAPSHOT.jar ../jenkins-bootstrap/docker_build/jarfiles
